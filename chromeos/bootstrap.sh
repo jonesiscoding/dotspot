fb331ca3983943c7bfc5858cd101645760e2d6ef
AMJones <aaron@devisicoding.com>
2020-05-03 08:26:30 -0500

    Initial ChromeOS commit

---
+++ chromeos/bootstrap.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+source "./sbin/functions.sh"
+source "./sbin/versions.sh"
+
+LINE="-----------------------------------------"
+
+STATUS_DEV=false
+STATUS_DOT=false
+
+DO_PHP=false
+DO_DOCKER=false
+
+BOXURL=https://github.com/humbug/box/releases/download/${BOXVERSION}/box.phar
+PLEASINGURL=https://github.com/jonesiscoding/pleasing-tools/releases/download/v${PLEASINGVERSION}/pleasing.phar
+
+lstart() {
+  becho "START: $1\n${LINE}"
+}
+
+lend() {
+  becho "${LINE}\nEND: $1\n"
+}
+
+for var in "$@"
+do
+  if [ $var == '-php' ]
+  then
+    DO_PHP=true
+  fi
+
+  if [ $var == '-docker' ]
+  then
+    DO_DOCKER=true
+  fi
+done
+
+echo -e " "
+
+# Install Development Tools
+if ask "Install Basic Development Tools?" Y; then
+  lstart "Installing Development Tools";
+  apt-get update && apt-get install -y --no-install-recommends \
+    wget \
+    nano \
+    git \
+    rsync \
+    make | indent
+  lend "Installing Development Tools"
+  STATUS_DEV=true
+fi
+
+# Install DotFiles
+if [ -f /usr/bin/rsync ]; then
+  if ask "Install Dot Files?" Y; then
+    lstart "Installing Dot Files..."
+    shopt -s dotglob
+    rsync --exclude ".git/" \
+      --exclude "bootstrap.sh" \
+      --exclude ".gitignore" \
+      --exclude "functions/" \
+      --exclude "README.md" \
+      --exclude "LICENSE" \
+      -avP --no-perms ./.* ~/;
+    lend "Installing Dot Files"
+  fi
+fi
+
+# Install VS Code
+if [ ! -f /usr/bin/code ]; then
+  if ask "Install VSCode?" Y; then
+    lstart "Installing VS Code..."
+    curl -L "https://go.microsoft.com/fwlink/?LinkID=760868" > vscode.deb
+    sudo apt install ./vscode.deb
+    rm vscode.deb
+    lend "Installing VS Code"
+  fi
+fi
+
+# Install PHPStorm
+if [ ! -f /opt/PhpStorm-*/bin/phpstorm.sh ]; then
+  if ask "Install VSCode?" Y; then
+    lstart "Installing PHPStorm..."
+    curl -L "https://download.jetbrains.com/webide/PhpStorm-${PHPSTORM_VER}.tar.gz" > PhpStorm.tar.gz
+    tar xzf ./PhpStorm.tar.gz -C /opt
+    rm ./PhpStorm*.tar.gz
+    lend "Installing PHPStorm"
+  fi
+fi
+
+# Install Docker
+if ask "Install Docker?" N; then
+  becho "START: Installing Docker..."
+  apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+  add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+  apt-get update && apt-get install docker-ce docker-ce-cli containerd.io
+  becho "END: Installing Docker"
+fi
+
+# Install PHP
+if ask "Install PHP and NPM?" Y; then
+  becho "START: Installing PHP...\n"
+  apt-get update && apt-get install -y --no-install-recommends \
+    locales \
+    libzip-dev \
+    libwebp-dev libjpeg62-turbo-dev libpng-dev \
+    libmagickwand-dev \
+    sassc \
+    jhead \
+    imagemagick \
+    libimage-exiftool-perl \
+    nodejs \
+    npm \
+    node-babel-cli \
+    node-babel-preset-es2015 \
+    php${PHP_VER} php${PHP_VER}-{bcmath,cli,common,curl,dev,exif,gd,intl,mbstring,mysql,opcache,sqlite3,xml,zip} php-pear php-imagick
+  pecl install APCu-5.1.17
+  locale-gen en_US.UTF-8 && locale-gen en_US
+  # Install Box
+  BOX_FILE=$(mktemp)
+  curl -o "$BOX_FILE" -SL "${BOXURL}"
+  mkdir -p /usr/local/bin/
+  mv ${BOX_FILE} /usr/local/bin/box; chmod 755 /usr/local/bin/box
+  # Install Pleasing
+  PLEASING_FILE=$(mktemp)
+  curl -o "$PLEASING_FILE" -SL "${PLEASINGURL}"
+  mv ${PLEASING_FILE} /usr/local/bin/pleasing
+  chmod 755 /usr/local/bin/pleasing
+  becho "\nEND: Installing PHP"
+fi
+
+if [ -f ~/.bash_profile ]; then
+  source ~/.bash_profile
+fi
+
+echo -e " "
---
+++ chromeos/sbin/functions.sh
@@ -0,0 +1,64 @@
+black="\033[0;30m"
+blackb="\033[1;30m"
+white="\033[0;37m"
+whiteb="\033[1;37m"
+red="\033[0;31m"
+redb="\033[1;31m"
+green="\033[0;32m"
+greenb="\033[1;32m"
+yellow="\033[0;33m"
+yellowb="\033[1;33m"
+blue="\033[0;34m"
+blueb="\033[1;34m"
+purple="\033[0;35m"
+purpleb="\033[1;35m"
+lightblue="\033[0;36m"
+lightblueb="\033[1;36m"
+end="\033[0m"
+
+becho() {
+  echo -e "${lightblueb}${1}${end}";
+}
+
+indent() { sed 's/^/  /'; }
+
+# This is a general-purpose function to ask Yes/No questions in Bash, either
+# with or without a default answer. It keeps repeating the question until it
+# gets a valid answer.
+
+ask() {
+    # https://djm.me/ask
+    local prompt default reply
+
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
+
+    while true; do
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -e -n "${purpleb}$1 ${end}[$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read reply </dev/tty
+
+        # Default?
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
+
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
